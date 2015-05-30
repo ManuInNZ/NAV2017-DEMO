579ee38a853d6e6f11405a6b50327bddd51bee5c
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2015-05-30 23:00:30 +0200

    split scripts

--- initialize.ps1
+++ initialize.ps1
@@ -5,12 +5,7 @@ param
       [string] $VMAdminUsername = $null,
       [string] $VMAdminPassword  = $null,
       [string] $Country = $null,
-      [string] $PublicMachineName = $null,
-      [string] $bingMapsKey = $null,
-      [string] $clickonce = $null,
-      [string] $powerBI = $null,
-      [string] $wordReporting = $null
-
+      [string] $PublicMachineName = $null
 )
 
 Set-ExecutionPolicy -ExecutionPolicy unrestricted -Force
@@ -43,22 +38,4 @@ try {
     . 'c:\DEMO\Initialize\install.ps1' 4> 'C:\DEMO\Initialize\install.log'
 } catch {
     Set-Content -Path "c:\DEMO\initialize-error.txt" -Value $_.Exception.Message
-}
-
-try {
-    if ($bingMapsKey -ne "No") {
-        ('$HardcodeBingMapsKey = "'+$bingMapsKey+'"') | Add-Content "c:\DEMO\BingMaps\HardcodeInput.ps1"
-        . 'c:\DEMO\BingMaps\install.ps1' 4> 'C:\DEMO\BingMaps\install.log'
-    }
-    if ($clickonce -eq "Yes") {
-        . 'c:\DEMO\Clickonce\install.ps1' 4> 'C:\DEMO\Clickonce\install.log'
-    }
-    if ($powerBI -eq "Yes") {
-        . 'c:\DEMO\PowerBI\install.ps1' 4> 'C:\DEMO\PowerBI\install.log'
-    }
-    if ($wordReporting -eq "Yes") {
-        . 'c:\DEMO\Word Reporting\install.ps1' 4> 'C:\DEMO\Word Reporting\install.log'
-    }
-} catch {
-    Set-Content -Path "c:\DEMO\install-error.txt" -Value $_.Exception.Message
 }
