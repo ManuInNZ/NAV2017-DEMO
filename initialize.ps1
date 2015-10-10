a26c0eb7dad77f2ecf512e98deb87aede5749a53
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2015-10-10 11:39:09 +0200

    Update initialize.ps1

--- initialize.ps1
+++ initialize.ps1
@@ -5,12 +5,14 @@ param
        [string]$VMAdminUsername = $null
       ,[string]$VMAdminPassword  = $null
       ,[string]$Country = $null
+      ,[string]$RestoreAndUseBakFile = "Default"
       ,[string]$PublicMachineName = $null
       ,[string]$bingMapsKey = $null
       ,[string]$clickonce = $null
       ,[string]$powerBI = $null
       ,[string]$Office365UserName = $null
       ,[string]$Office365Password = $null
+      ,[string]$Office365CreatePortal = "Yes"
 )
 
 Set-ExecutionPolicy -ExecutionPolicy unrestricted -Force
@@ -30,12 +32,13 @@ $failure = $false
 
 try {
     # Initialize Virtual Machine
-    ('$HardcodeLanguage = "'+$Country.Substring(0,2)+'"')      | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
-    ('$HardcodeNavAdminUser = "'+$NAVAdminUser+'"')            | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
-    ('$HardcodeNavAdminPassword = "'+$NAVAdminPassword+'"')    | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
-    ('$HardcodeCloudServiceName = "'+$CloudServiceName+'"')    | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
-    ('$HardcodePublicMachineName = "'+$PublicMachineName+'"')  | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
-    ('$HardcodecertificatePfxFile = "default"')                | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
+    ('$HardcodeLanguage = "'+$Country.Substring(0,2)+'"')          | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
+    ('$HardcodeNavAdminUser = "'+$NAVAdminUser+'"')                | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
+    ('$HardcodeNavAdminPassword = "'+$NAVAdminPassword+'"')        | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
+    ('$HardcodeRestoreAndUseBakFile = "'+$RestoreAndUseBakFile'"') | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
+    ('$HardcodeCloudServiceName = "'+$CloudServiceName+'"')        | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
+    ('$HardcodePublicMachineName = "'+$PublicMachineName+'"')      | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
+    ('$HardcodecertificatePfxFile = "default"')                    | Add-Content "c:\DEMO\Initialize\HardcodeInput.ps1"
     . 'c:\DEMO\Initialize\install.ps1' 4> 'C:\DEMO\Initialize\install.log'
 } catch {
     Set-Content -Path "c:\DEMO\initialize\error.txt" -Value $_.Exception.Message
@@ -48,16 +51,16 @@ prompt for credentials:i:1')
 
 if ($Office365UserName -ne "No") {
     try {
-        ('$HardcodeNavAdminUser = "default"')                                      | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeSharePointAdminLoginname = "'+$Office365UserName + '"')         | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeSharePointAdminPassword = "'+$Office365Password + '"')          | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeCreateSharePointPortal = "default"')                            | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeSharePointUrl = "default"')                                     | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeSharePointSite = "' + ($PublicMachineName.Split('.')[0]) + '"') | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeSharePointLanguage = "default"')                                | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeSharePointTimezoneId = "default"')                              | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeSharePointAppCatalogUrl = "default"')                           | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
-        ('$HardcodeSharePointMultitenant = "No"')                                  | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeNavAdminUser = "default"')                                    | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeSharePointAdminLoginname = "'+$Office365UserName+'"')         | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeSharePointAdminPassword = "'+$Office365Password+'"')          | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeCreateSharePointPortal = "'+$Office365CreatePortal+'"')       | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeSharePointUrl = "default"')                                   | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeSharePointSite = "' + ($PublicMachineName.Split('.')[0])+'"') | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeSharePointLanguage = "default"')                              | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeSharePointTimezoneId = "default"')                            | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeSharePointAppCatalogUrl = "default"')                         | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
+        ('$HardcodeSharePointMultitenant = "No"')                                | Add-Content "c:\DEMO\O365 Integration\HardcodeInput.ps1"
         . 'c:\DEMO\O365 Integration\install.ps1' 4> 'C:\DEMO\O365 Integration\install.log'
     } catch {
         Set-Content -Path "c:\DEMO\O365 Integration\error.txt" -Value $_.Exception.Message
